76a938d1ccfc5b6eea6d20725e8b8ee0016a0def
AkilaManu <akilamanujith@gmail.com>
2025-06-12 15:17:26 +0530

    done simple gui for the chat app

--- src/main/java/com/example/chatter/client/Utilities.java
+++ src/main/java/com/example/chatter/client/Utilities.java
@@ -2,8 +2,15 @@ package com.example.chatter.client;
 
 import java.awt.Color;
 
+import javax.swing.border.EmptyBorder;
+
 public class Utilities {
-    public static final Color TRANSPARENT_COLOR = new Color(0,0,0,0);
-    public static final Color PRIMARY = new Color(0,0,0,0);
-    public static final Color SECOND = new Color(0,0,0,0);
+    public static final Color TRANSPARENT_COLOR = new Color(0, 0, 0, 0); // Transparent version of PRIMARY_COLOR
+    public static final Color PRIMARY_COLOR = Color.decode("#819A91");
+    public static final Color SECONDARY_COLOR = Color.decode("#D1D8BE");
+    public static final Color TEXT_COLOR = Color.decode("#000000");
+
+    public static EmptyBorder addPadding(int top,int left, int bottom,int right){
+        return new EmptyBorder(top,left,bottom,right);
+    }
 }
